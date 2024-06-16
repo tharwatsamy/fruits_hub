0888abc1ef6d41c2da3ad2da5e964b8ad63c5169
tharwatsamy <tharwatsamy2004@gmail>
2024-06-17 02:38:02 +0300

    Add signinWithEmailAndPassword method to AuthRepo interface

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -6,4 +6,7 @@ import '../entites/user_entity.dart';
 abstract class AuthRepo {
   Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
       String email, String password, String name);
+
+  Future<Either<Failure, UserEntity>> signinWithEmailAndPassword(
+      String email, String password, String name);
 }
