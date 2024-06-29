4209eb220229591448a31a702a3ff1dc829a50c6
tharwatsamy <tharwatsamy2004@gmail>
2024-06-29 03:27:58 +0300

    Add Firestore functionality to FireStoreService class

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'data_service.dart';
 
 class FireStoreService implements DatabaseService {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   @override
-  Future<void> addData({required String path, required Map<String, dynamic> data}) {
-   
+  Future<void> addData(
+      {required String path, required Map<String, dynamic> data}) async {
+    await firestore.collection(path).add(data);
   }
 }
