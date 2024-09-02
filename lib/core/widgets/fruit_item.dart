596434fd22346f88aa7f2349f06548ba1446967c
tharwat <tharwatsamy2004@gmail.com>
2024-09-02 13:17:59 +0300

    build fruit item

---
+++ lib/core/widgets/fruit_item.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:svg_flutter/svg.dart';
+
+class FruitItem extends StatelessWidget {
+  const FruitItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: ShapeDecoration(
+        color: const Color(0xFFF3F5F7),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite_outline,
+                )),
+          ),
+          Positioned.fill(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Image.asset(
+                  Assets.imagesWatermelonTest,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                ListTile(
+                  title: const Text(
+                    'بطيخ',
+                    textAlign: TextAlign.right,
+                    style: TextStyles.semiBold16,
+                  ),
+                  subtitle: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '30جنية ',
+                          style: TextStyles.bold13.copyWith(
+                            color: AppColors.secondaryColor,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '/',
+                          style: TextStyles.bold13.copyWith(
+                            color: AppColors.lightSecondaryColor,
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' ',
+                          style: TextStyles.semiBold13.copyWith(
+                            color: AppColors.secondaryColor,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'الكيلو',
+                          style: TextStyles.semiBold13.copyWith(
+                            color: AppColors.lightSecondaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.right,
+                  ),
+                  trailing: const CircleAvatar(
+                    backgroundColor: AppColors.primaryColor,
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
