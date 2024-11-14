7b260a6a8ece80a58a8287fd7cf72f28f30b2500
tharwat <tharwatsamy2004@gmail.com>
2024-11-14 18:31:37 +0200

    create payment item

---
+++ lib/core/utils/app_decorations.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppDecorations {
+  static var greyBoxDecoration = ShapeDecoration(
+    color: const Color(0x7FF2F3F3),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+  );
+}
---
+++ lib/features/checkout/presentation/views/widgets/payment_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_decorations.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+
+class PaymentItem extends StatelessWidget {
+  const PaymentItem({super.key, required this.tile, required this.child});
+
+  final String tile;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '$tile:',
+          style: TextStyles.bold13,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Container(
+          decoration: AppDecorations.greyBoxDecoration,
+          child: child,
+        ),
+      ],
+    );
+  }
+}
